a5a4dafa449ed2d890e73ec5568894a9f6430177
lhannest <lhannest@gmail.com>
2017-06-02 07:40:05 -0700

    Things were set up where the evidence API call was being sent to every active beacon, but this is pointless because only one of the beacons can possibly have what we're looking for. Now we're keeping track of which beacon a statement came from, and querying only that beacon for evidence for that statement.

--- database/src/main/java/bio/knowledge/database/repository/ConceptRepository.java
+++ database/src/main/java/bio/knowledge/database/repository/ConceptRepository.java
@@ -145,9 +145,10 @@ public interface ConceptRepository extends GraphRepository<Neo4jConcept> {
 			" WITH " +
 			" 	SIZE(FILTER(x IN {filter} WHERE LOWER(concept.name) CONTAINS LOWER(x))) AS num_name_matches, " +
 			" 	SIZE(FILTER(x IN {filter} WHERE LOWER(concept.synonyms) CONTAINS LOWER(x))) AS num_syn_matches, " +
+			" 	SIZE(FILTER(x IN {filter} WHERE LOWER(concpet.description) CONTAISN LOWER(x) AS num_desc_matches, " +
 			" 	concept AS concept " +
 			" WHERE ( " +
-			" 	num_name_matches > 0 OR num_syn_matches > 0 " +
+			" 	num_name_matches > 0 OR num_syn_matches > 0 OR num_desc_matches > 0 " +
 			" ) AND ( "+
 			" 	{semanticGroups} IS NULL OR SIZE({semanticGroups}) = 0 OR " +
 			" 	ANY (x IN {semanticGroups} WHERE LOWER(concept.semanticGroup) = LOWER(x)) " +
